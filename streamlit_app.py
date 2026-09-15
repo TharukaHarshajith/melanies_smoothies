d3c9e4ea2d257ab0bc5b894ff63ca917349d3657
Tharuka Harshajith <137327327+TharukaHarshajith@users.noreply.github.com>
2026-09-15 11:03:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ name_on_order = st.text_input(
 
 st.write("The name on your Smoothie will be:", name_on_order )
 
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 
