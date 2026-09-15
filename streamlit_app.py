353cf01fa75626c3c74b16d01a65486469b59fad
Tharuka Harshajith <137327327+TharukaHarshajith@users.noreply.github.com>
2026-09-15 11:01:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,16 @@ from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
 
 name_on_order = st.text_input(
     "Name on Smoothie",
 )
 
 st.write("The name on your Smoothie will be:", name_on_order )
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
